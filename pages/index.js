eafd02751d4816e1e9c0b2a6f3fdf493905b9556
poyrazavsever <poyrazavsever@hotmail.com>
2025-05-01 12:09:36 +0300

    feat: added interface to index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Head from "next/head";
+import ChatInterface from "../components/ChatInterface";
+
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Head>
+        <title>Gemini Chatbot</title>
+      </Head>
+      <main>
+        <h1 className="text-2xl text-center mt-4">Gemini Chatbot</h1>
+        <ChatInterface />
+      </main>
+    </>
   );
 }
